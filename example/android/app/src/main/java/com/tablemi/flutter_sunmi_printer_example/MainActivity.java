e4a13e21c7a1e234928ca1006258a429e4348904
macbook <s2yedftouh@gmail.com>
2023-12-16 20:41:25 +0200

    pro

--- example/android/app/src/main/java/com/tablemi/flutter_sunmi_printer_example/MainActivity.java
+++ example/android/app/src/main/java/com/tablemi/flutter_sunmi_printer_example/MainActivity.java
@@ -1,6 +1,9 @@
 package com.tablemi.flutter_sunmi_printer_example;
 
+import android.os.Bundle;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
 }
